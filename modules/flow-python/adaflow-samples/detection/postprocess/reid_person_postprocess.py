1b4c333b946aa0ee27a0bf6e104036c14403f66c
静瑶 <jingyao.ww@alibaba-inc.com>
2023-03-13 16:08:39 +0800

    存量迁移：行人重识别

---
+++ modules/flow-python/adaflow-samples/detection/postprocess/reid_person_postprocess.py
@@ -0,0 +1,29 @@
+"""
+reid person in detection post process
+"""
+from adaflow.utils.video_frame import AVDataPacket
+import numpy as np
+import gi
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
+gi.require_version('Gst', '1.0')
+gi.require_version('GstBase', '1.0')
+gi.require_version('GObject', '2.0')
+
+class ReidPersonPostprocess:
+    def __init__(self):
+        pass
+    def postprocess(self, frames: AVDataPacket, kwargs):
+        ##frame by frame
+        for frame in frames:
+            feat_1 = frame.get_json_meta('model1')['img_embedding'][0]
+            feat_2 = frame.get_json_meta('model2')['img_embedding'][0]
+            feat_1 = np.array(feat_1)
+            feat_2 = np.array(feat_2)
+            feat_norm_1 = feat_1 / np.linalg.norm(feat_1)
+            feat_norm_2 = feat_2 / np.linalg.norm(feat_2)
+            score = np.dot(feat_norm_1, feat_norm_2)
+            logging.info(f'cosine score is: {score}')
+
+            return True
--- modules/flow-python/adaflow-samples/detection/test_detection.sh
+++ modules/flow-python/adaflow-samples/detection/test_detection.sh
@@ -12,3 +12,12 @@ decodebin ! videoconvert ! videoscale ! video/x-raw,format=RGB ! \
 mass_model task=domain-specific-object-detection id = damo/cv_tinynas_human-detection_damoyolo ! \
 mass_model_post input=/Users/jingyao/Documents/23S1/test-img/src/break_in_count_deploy.yaml module=/Users/jingyao/Documents/23S1/AdaFlow/modules/flow-gst-plugin/flow-detection-samples/postprocess/break_in_det_postprocess.py class= BreakInDetPostprocess function = postprocess ! \
 videoconvert ! videoscale ! video/x-raw,format=I420 ! x264enc ! mp4mux ! filesink location=/Users/jingyao/Documents/23S1/test-img/res/tf_result.mp4
+
+#reid-person
+gst-launch-1.0 filesrc location=/Users/jingyao/Documents/23S1/test-img/src/image_reid_person.jpg ! \
+decodebin ! videoconvert ! videoscale ! video/x-raw,format=RGB ! \
+mass_model task=image-reid-person id = damo/cv_passvitb_image-reid-person_market meta-key=model1 ! \
+meta_aggregator name = mixer ! mass_model_post input=/Users/jingyao/Documents/23S1/test-img/src/break_in_count_deploy.yaml module=/Users/jingyao/Documents/23S1/AdaFlow/modules/flow-python/adaflow-samples/detection/postprocess/reid_person_postprocess.py class= ReidPersonPostprocess ! fakesink \
+filesrc location=/Users/jingyao/Documents/23S1/test-img/src/image_reid_person.jpg ! \
+decodebin ! videoconvert ! videoscale ! video/x-raw,format=RGB ! \
+mass_model task=image-reid-person id = damo/cv_passvitb_image-reid-person_market meta-key=model2 ! mixer.
