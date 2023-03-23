7bd6369af122091221e0bd03763e73d2dbfaa9e7
静瑶 <jingyao.ww@alibaba-inc.com>
2023-03-23 14:16:37 +0800

    modelscope_pipeline support more task:change BaseBaseTransform to Gst.Element for buffer resize such as SR

--- modules/adaflow-gst-plugin/python/flow_modelscope_pipeline.py
+++ modules/adaflow-gst-plugin/python/flow_modelscope_pipeline.py
@@ -3,9 +3,10 @@
     Run modelscope pipeline and produce result data.
 """
 from modelscope.pipelines import pipeline
-from adaflow.av.utils import gst_video_format_from_string, get_num_channels,NumpyArrayEncoder
+from modelscope.outputs import OutputKeys
+from adaflow.av.utils import gst_video_format_from_string, get_num_channels
 from adaflow.av.metadata.flow_json_meta import flow_meta_add_key
-from gi.repository import Gst, GObject, GstBase
+from gi.repository import Gst, GObject, GstBase, GstVideo
 from absl import logging
 import numpy as np
 
@@ -15,10 +16,7 @@ gi.require_version('Gst', '1.0')
 gi.require_version('GstBase', '1.0')
 gi.require_version('GstVideo', '1.0')
 
-
-FORMATS = "{RGB, RGBA, I420, NV12, NV21}"
-
-class FlowMassModelPlugin(GstBase.BaseTransform):
+class FlowMassModelPlugin(Gst.Element):
 
     GST_PLUGIN_NAME = 'flow_modelscope_pipeline'
 
@@ -30,43 +28,50 @@ class FlowMassModelPlugin(GstBase.BaseTransform):
     __gsttemplates__ = (Gst.PadTemplate.new("src",
                                             Gst.PadDirection.SRC,
                                             Gst.PadPresence.ALWAYS,
-                                            # Set to RGB format
-                                            Gst.Caps.from_string(
-                                                f"video/x-raw,format={FORMATS}")),
+                                            Gst.Caps.new_any()),
                         Gst.PadTemplate.new("sink",
                                             Gst.PadDirection.SINK,
                                             Gst.PadPresence.ALWAYS,
-                                            # Set to RGB format
-                                            Gst.Caps.from_string(
-                                                f"video/x-raw,format={FORMATS}")))
+                                            Gst.Caps.new_any()))
+
+    _sinkpadtemplate = __gsttemplates__[1]
+    _srcpadtemplate = __gsttemplates__[0]
+
 
     __gproperties__ = {
-                       "task": (GObject.TYPE_STRING,  # type
-                                            "model task",  # nick
-                                            "maas tasks",  # blurb
-                                            "",  # default
-                                            GObject.ParamFlags.READWRITE
-                                            # flags
-                                            ),
-                       "id": (GObject.TYPE_STRING,  # type
-                                          "model id on the maas",  # nick
-                                          "model id",  # blurb
-                                          "",  # default
-                                          GObject.ParamFlags.READWRITE  # flags
-                                          ),
-                       "input": (GObject.TYPE_STRING,  # type
-                                  "image or video input path",  # nick
-                                  "input path",  # blurb
-                                  "",  # default
-                                  GObject.ParamFlags.READWRITE  # flags
-                                  ),
+        "task": (GObject.TYPE_STRING,  # type
+                 "model task",  # nick
+                 "maas tasks",  # blurb
+                 "",  # default
+                 GObject.ParamFlags.READWRITE
+                 # flags
+                 ),
+        "id": (GObject.TYPE_STRING,  # type
+               "model id on the maas",  # nick
+               "model id",  # blurb
+               "",  # default
+               GObject.ParamFlags.READWRITE  # flags
+               ),
+        "input": (GObject.TYPE_STRING,  # type
+                  "image or video input path",  # nick
+                  "input path",  # blurb
+                  "",  # default
+                  GObject.ParamFlags.READWRITE  # flags
+                  ),
 
-                       "meta-key": (GObject.TYPE_STRING,  # type
-                                 "the keyword of the results which attach to the GstBuffer",  # nick
-                                 "the keyword of the results",  # blurb
-                                 "modelout",  # default
-                                 GObject.ParamFlags.READWRITE  # flags
-                                 ),
+        "meta-key": (GObject.TYPE_STRING,  # type
+                     "the keyword of the results which attach to the GstBuffer",  # nick
+                     "the keyword of the results",  # blurb
+                     "modelout",  # default
+                     GObject.ParamFlags.READWRITE  # flags
+                     ),
+
+        "add-meta": (GObject.TYPE_BOOLEAN,  # type
+                     "whether to add metadata at buffer",  # nick
+                     "whether to add metadata at buffer",  # blurb
+                     True,  # default
+                     GObject.ParamFlags.READWRITE  # flags
+                     ),
     }
 
     def __init__(self):
@@ -76,7 +81,16 @@ class FlowMassModelPlugin(GstBase.BaseTransform):
         self.task = None
         self.id = None
         self.input = None
+        self.add_meta = True
         self.meta_key = 'modelout'
+
+        self.sinkpad = Gst.Pad.new_from_template(self._sinkpadtemplate, 'sink')
+        self.sinkpad.set_chain_function_full(self.chainfunc, None)
+        self.sinkpad.set_event_function_full(self.eventfunc, None)
+
+        self.add_pad(self.sinkpad)
+        self.srcpad = Gst.Pad.new_from_template(self._srcpadtemplate, 'src')
+        self.add_pad(self.srcpad)
 
 
     def do_get_property(self, prop: GObject.GParamSpec):
@@ -88,6 +102,8 @@ class FlowMassModelPlugin(GstBase.BaseTransform):
             return self.input
         elif prop.name == 'meta-key':
             return self.meta_key
+        elif prop.name == 'add-meta':
+            return self.add_meta
         else:
             raise AttributeError('unknown property %s' % prop.name)
 
@@ -96,47 +112,64 @@ class FlowMassModelPlugin(GstBase.BaseTransform):
             self.task = value
         elif prop.name == 'id':
             self.id = value
+            ##init model
+            self.maas_pipeline = pipeline(self.task, model=self.id)
         elif prop.name == 'input':
             self.input = value
         elif prop.name == 'meta-key':
             self.meta_key = value
+        elif prop.name == 'add-meta':
+            self.add_meta = value
         else:
             raise AttributeError('unknown property %s' % prop.name)
 
-    def do_set_caps(self, incaps: Gst.Caps, outcaps: Gst.Caps) -> bool:
-        struct = incaps.get_structure(0)
-        self.width = struct.get_int("width").value
-        self.height = struct.get_int("height").value
-        video_format = gst_video_format_from_string(struct.get_value('format'))
-        self.channel = get_num_channels(video_format)
-
+    def eventfunc(self, pad, parent, event):
+        # #check pars
         if self.task is None or self.id is None:
             raise ValueError(f'id = {self.id} or task = {self.task} is error ')
 
-        #build maas pipeline
-        self.maas_pipeline = pipeline(self.task, model=self.id)
+        return self.srcpad.push_event(event)
 
-        return True
+    def chainfunc(self, pad: Gst.Pad, parent, buffer: Gst.Buffer) -> Gst.FlowReturn:
+        ##buffer-in-info
+        incaps = self.sinkpad.get_current_caps()
+        struct = incaps.get_structure(0)
+        width_in = struct.get_int("width").value
+        height_in = struct.get_int("height").value
+        video_format = gst_video_format_from_string(struct.get_value('format'))
+        channel_in = get_num_channels(video_format)
+        ##make buffer writable after tee
+        buffer = Gst.Buffer.copy_deep(buffer)
 
-    def do_transform_ip(self, buffer: Gst.Buffer) -> Gst.FlowReturn:
-        try:
-            with buffer.map(Gst.MapFlags.READ | Gst.MapFlags.WRITE) as info:
-                image = np.ndarray(
-                    shape=(self.height, self.width, self.channel),
-                    dtype=np.uint8, buffer=info.data)
-                if self.input is not None:
-                    det_res = self.maas_pipeline(self.input)
-                    logging.debug(f'{det_res}')
-                else:
-                    det_res = self.maas_pipeline(image)
+        with buffer.map(Gst.MapFlags.READ | Gst.MapFlags.WRITE) as info:
 
-                flow_meta_add_key(buffer, det_res, self.meta_key)
+            image = np.ndarray(shape=(height_in, width_in, channel_in), dtype=np.uint8, buffer=info.data)
 
-                return Gst.FlowReturn.OK
-        except Gst.MapError as e:
-            Gst.error("Mapping error: %s" % e)
-            return Gst.FlowReturn.ERROR
+            if self.input is not None:
+                infer_res = self.maas_pipeline(self.input)
+                logging.debug(f'{infer_res}')
+            else:
+                infer_res = self.maas_pipeline(image)
 
+            if(self.add_meta):
+                flow_meta_add_key(buffer, infer_res, self.meta_key)
+                return self.srcpad.push(buffer)
+            else:
+                infer_image = infer_res[OutputKeys.OUTPUT_IMG]
+                ##set new outcaps
+                height_out, width_out, channel_out = infer_image.shape
+                if(height_out!=height_in or width_out!=width_in):
+                    outcaps = self.srcpad.get_current_caps()
+                    VideoInfo = GstVideo.VideoInfo.new_from_caps(outcaps)
+                    VideoInfo.width = width_out
+                    VideoInfo.height = height_out
+                    outcaps = GstVideo.VideoInfo.to_caps(VideoInfo)
+                    self.srcpad.set_caps(outcaps)
+
+                infer_data = infer_image.tobytes()
+                newbuf = Gst.Buffer.new_allocate(None, len(infer_data), None)
+                newbuf.fill(0, infer_data)
+                return self.srcpad.push(newbuf)
 
 GObject.type_register(FlowMassModelPlugin)
 __gstelementfactory__ = (FlowMassModelPlugin.GST_PLUGIN_NAME,
