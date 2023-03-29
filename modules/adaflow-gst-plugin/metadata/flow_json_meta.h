1b12d9f1805cc5f56718edad83666fe5184a4e6b
静瑶 <jingyao.ww@alibaba-inc.com>
2023-03-29 11:44:56 +0800

    det info

--- modules/adaflow-gst-plugin/metadata/flow_json_meta.h
+++ modules/adaflow-gst-plugin/metadata/flow_json_meta.h
@@ -19,8 +19,8 @@ typedef struct _GstFLOWJSONMeta GstFLOWJSONMeta;
  * @brief This struct represents JSON metadata and contains instance of parent GstMeta and message
  */
 struct _GstFLOWJSONMeta {
-  GstMeta meta;   /**< parent GstMeta */
-  gchar *message; /**< C-string message */
+  GstMeta meta;
+  gchar *message;
 };
 
 const GstMetaInfo *gst_flow_json_meta_get_info(void);
@@ -31,27 +31,11 @@ GType gst_flow_json_meta_api_get_type(void);
 #define GST_FLOW_JSON_META_ITERATE(buf, state)                                                                          \
     ((GstFLOWJSONMeta *)gst_buffer_iterate_meta_filtered(buf, state, gst_flow_json_meta_api_get_type()))
 
-/**
- * @def GST_FLOW_JSON_META_ADD
- * @brief This macro attaches new _GstFLOWJSONMeta instance to passed buf
- * @param buf GstBuffer* to which metadata will be attached
- * @return _GstFLOWJSONMeta* of the newly added instance attached to buf
- */
 #define GST_FLOW_JSON_META_ADD(buf) ((GstFLOWJSONMeta *)gst_buffer_add_meta(buf, gst_flow_json_meta_get_info(), NULL))
 
-/**
- * @brief This function returns message field of _GstFLOWJSONMeta
- * @param meta _GstFLOWJSONMeta* to retrieve message of
- * @return C-style string with message
- */
+
 gchar *get_json_message(GstFLOWJSONMeta *meta);
 
-/**
- * @brief This function sets message field of _GstFLOWJSONMeta
- * @param meta _GstFLOWJSONMeta* to set message
- * @param message message
- * @return void
- */
 void set_json_message(GstFLOWJSONMeta *meta, const gchar *message);
 
 GstFLOWJSONMeta * gst_buffer_add_json_info_meta(GstBuffer *buffer, const gchar *message);
