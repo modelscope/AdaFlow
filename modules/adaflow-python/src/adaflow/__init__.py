2b1a4d2b5b7521299bd2f9ae9643fbeb25edd96c
long.qul <long.qul@alibaba-icnc.om>
2023-04-26 15:50:54 +0800

    feat: add request id to log

--- modules/adaflow-python/src/adaflow/__init__.py
+++ modules/adaflow-python/src/adaflow/__init__.py
@@ -1,2 +1,2 @@
 from .av.pipeline.pipeline_factory import PipelineFactory
-from .av.serving.http_server import http_pipeline_server
+from adaflow.av.serving.http.http_server import http_pipeline_server
